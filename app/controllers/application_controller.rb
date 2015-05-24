2820291bdc27036be3dee301b0eaa30d4c58cdff
MaxMeira <maxdawis@gmail.com>
2015-05-24 14:56:35 -0300

    Adding information to Users and criate titles davise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-    render text: "SOA UFBA!"
-  end
+#  def hello
+#    render text: "SOA UFBA!"
+#  end
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+      def configure_permitted_parameters
+          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :matriculation, :email, :password) }
+          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :matriculation, :email, :password, :current_password) }
+      end
 end
