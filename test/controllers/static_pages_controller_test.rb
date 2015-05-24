428f124011f0c9f80eee4ac3a6b0c774e365afc6
MaxMeira <maxdawis@gmail.com>
2015-05-24 00:47:45 -0300

    Finish static pages and simple test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,31 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | SOA - Sistema de Orientação Acadêmica da Ufba!"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Ajuda | SOA - Sistema de Orientação Acadêmica da Ufba!"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "Sobre | SOA - Sistema de Orientação Acadêmica da Ufba!"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contato | SOA - Sistema de Orientação Acadêmica da Ufba!"
   end
 
   test "should get news" do
     get :news
     assert_response :success
+    assert_select "title", "News | SOA - Sistema de Orientação Acadêmica da Ufba!"
   end
 
 end
